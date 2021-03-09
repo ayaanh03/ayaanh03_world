b9f01cf999c059dc13af4f62241f64c09bf505ba
Eshita Banerjee <ebanerje@andrew.cmu.edu>
2021-03-08 23:28:08 -0500

    step 7

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
 	has_many :photos
+	# Relationships
+  belongs_to :article
 
 	scope :active, 			 -> { where('active = ?', true) }
   scope :alphabetical, -> { order('name') }
